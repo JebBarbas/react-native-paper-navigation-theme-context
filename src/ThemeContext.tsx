00158b7e22c87d5263cb2a6fddd58067b2cb59d8
Enrique Barboza <enriquebarbozajeb@gmail.com>
2021-08-27 19:38:19 -0700

    fix: Removed react from dependencies, it was causing a double react 🧪

--- src/ThemeContext.tsx
+++ src/ThemeContext.tsx
@@ -3,7 +3,9 @@ import React, {
     createContext, 
     useContext, 
     useState,
-    useEffect
+    useEffect,
+    useCallback,
+    useMemo,
 } from 'react';
 import { useColorScheme } from 'react-native';
 import { MixedTheme, OptionalMixedTheme, DefaultTheme, DarkTheme, overrideTheme } from './themes';
@@ -50,16 +52,16 @@ export const useTheme = ():ContextValue => {
 
 const ThemeProvider:FC<ThemeProviderProps> = ({children, override}) => {  
     // START MEMO CONSTANTS //
-    const COLOR_SCHEME:ColorSchemeNameObject = {LIGHT: 'light',DARK: 'dark',DEFAULT: 'default'}
+    const COLOR_SCHEME:ColorSchemeNameObject = useMemo(() => ({LIGHT: 'light',DARK: 'dark',DEFAULT: 'default'}), [])
 
     const localColorSchemeKey = 'localColorSheme'
     // END MEMO CONSTANTS //
 
     // START THEME CREATOR //
-    const createAppTheme =(colorSchemeValue:ColorSchemeName):MixedTheme => {
+    const createAppTheme = useCallback((colorSchemeValue:ColorSchemeName):MixedTheme => {
         const UsedTheme = colorSchemeValue === COLOR_SCHEME.DARK ? DarkTheme : DefaultTheme
         return overrideTheme(UsedTheme, override)
-    }
+    }, [COLOR_SCHEME, override])
     // END THEME CREATOR //
 
     // START COMPONENT STATE //
@@ -69,7 +71,7 @@ const ThemeProvider:FC<ThemeProviderProps> = ({children, override}) => {
     // END COMPONENT STATE
 
     // START ASYNC STORAGE //
-    const getLocalColorScheme = async () => {
+    const getLocalColorScheme = useCallback(async () => {
         try{
             const localColorScheme = await AsyncStorage.getItem(localColorSchemeKey)
             if(localColorScheme === COLOR_SCHEME.LIGHT) return COLOR_SCHEME.LIGHT
@@ -80,7 +82,7 @@ const ThemeProvider:FC<ThemeProviderProps> = ({children, override}) => {
             console.warn('Error trying to get the local color scheme')
             return COLOR_SCHEME.DEFAULT
         }
-    }
+    }, [COLOR_SCHEME])
 
     const saveLocalColorScheme = async (colorSchemeValue:ColorSchemeName) => {
         try{
@@ -101,7 +103,7 @@ const ThemeProvider:FC<ThemeProviderProps> = ({children, override}) => {
     // END ASYNC STORAGE //
 
     // START STATE UPDATE //
-    const updateTheme = async () => {
+    const updateTheme = useCallback(async () => {
         try{
             let theme:MixedTheme
             const force = await getLocalColorScheme()
@@ -119,7 +121,7 @@ const ThemeProvider:FC<ThemeProviderProps> = ({children, override}) => {
         catch(err){
             console.error(err)
         }
-    }
+    }, [COLOR_SCHEME, createAppTheme, deviceColorScheme, getLocalColorScheme])
 
     const updateThemeWith = async (colorSchemeValue:ColorSchemeName) => {
         await saveLocalColorScheme(colorSchemeValue)
@@ -140,7 +142,7 @@ const ThemeProvider:FC<ThemeProviderProps> = ({children, override}) => {
     useEffect(()=>{
         updateTheme()
         return () => { 0 }
-    },[])
+    },[updateTheme])
 
     const value = {
         COLOR_SCHEME,
